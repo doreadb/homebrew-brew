141c9f50bd78796b002ffd3af476094aeda0c1ff
YuKun Liu <41265098+mrxiaozhuox@users.noreply.github.com>
2021-11-13 10:40:34 +0800

    feat: commit code

---
+++ Formula/dorea.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Dorea < Formula
+  desc "A Key-Value data storage system. - dorea db"
+  homepage "https://dorea.mrxzx.info"
+  url "https://github.com/mrxiaozhuox/dorea/releases/download/v0.3.0/dorea_v0.3.0-macos-server.tar.gz"
+  sha256 "a978da9d5ed6bbc853b40003e0bb50b6fdc624bd0645e18b4512196f64038671"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "Dorea"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test dorea`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
+
